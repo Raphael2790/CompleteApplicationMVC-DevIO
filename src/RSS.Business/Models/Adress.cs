f737b02734a71cdf7bab2d81219420d0d389b86a
Raphael Silvestre dos Santos <raphael.silvestre@animaeducacao.com.br>
2021-06-08 09:13:56 -0300

    Creating default folders and project schemas

---
+++ src/RSS.Business/Models/Adress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace RSS.Business.Models
+{
+    public class Adress : Entity
+    {
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string ZipCode { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public Guid SupplierId { get; set; }
+    }
+}
---
+++ src/RSS.CompleteApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RSS.CompleteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
